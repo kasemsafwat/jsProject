f99354d340effeda5dee72192d09068b64d1812e
Ksafwat <kasemsafwat.96@gmail.com>
2024-12-27 00:37:01 +0200

    add search button to shop page

--- js/script.js
+++ js/script.js
@@ -180,3 +180,16 @@ const displayData = async () => {
 };
 
 displayData();
+
+/************************************************************************************* */
+
+const toggleSearch = (search, button) => {
+  const searchBar = document.getElementById(search),
+    searchButton = document.getElementById(button);
+
+  searchButton.addEventListener("click", () => {
+    // We add the show-search class, so that the search ba
+    searchBar.classList.toggle("show-search");
+  });
+};
+toggleSearch("search-bar", "search-button-id");
